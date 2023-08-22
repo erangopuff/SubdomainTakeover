aa1240d9b5d9928f263d907885dd5b69832615b5
erangopuff <122721617+erangopuff@users.noreply.github.com>
2023-08-22 17:02:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,7 @@ def takeover():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['POST'])
 def hello():
